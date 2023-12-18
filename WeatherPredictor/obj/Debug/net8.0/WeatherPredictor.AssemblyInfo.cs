bbc0e4702fa72b03b29b9f2da5a81cd3dc308cd6
Ali Chowdhury <72588555+AliChowdhuryA@users.noreply.github.com>
2023-12-17 19:04:21 -0500

    update: repo things

--- WeatherPredictor/obj/Debug/net8.0/WeatherPredictor.AssemblyInfo.cs
+++ WeatherPredictor/obj/Debug/net8.0/WeatherPredictor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherPredictor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b28ccdb7ea80d1f1db814530b70b745c174f3f15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherPredictor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherPredictor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
