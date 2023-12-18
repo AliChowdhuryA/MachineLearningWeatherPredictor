720acba3d0ce1fb30b19bb2e0328c4cba954867a
Ali Chowdhury <72588555+AliChowdhuryA@users.noreply.github.com>
2023-12-17 19:29:35 -0500

    update: sending Rest Request to weather API

--- WeatherPredictor/Program.cs
+++ WeatherPredictor/Program.cs
@@ -1,9 +1,41 @@
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
+
+// Initialize the HttpClient object
+var client = new HttpClient();
+
+try
+{
+    // Send a GET request to the weather API
+    var response = await client.GetAsync("https://api.blueskyapi.io");
+
+    // Ensure the request was successful
+    response.EnsureSuccessStatusCode();
+
+    // Read the response body as a string
+    var responseBody = await response.Content.ReadAsStringAsync();
+
+    // Deserialize the response body into a WeatherData object
+    var weatherData = JsonSerializer.Deserialize<WeatherData>(responseBody);
+}
+catch (HttpRequestException e)
+{
+    Console.WriteLine("\nException Caught!");
+    Console.WriteLine("Message :{0} ", e.Message);
+}
+catch (JsonException e)
+{
+    Console.WriteLine("\nException Caught!");
+    Console.WriteLine("Message :{0} ", e.Message);
+}
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
---
+++ WeatherPredictor/WeatherData.cs
@@ -0,0 +1,8 @@
+public class WeatherData
+{
+    public double Temperature { get; set; }
+    public double Humidity { get; set; }
+    public string Condition { get; set; }
+    // Add more properties as needed based on the API response
+}
+
